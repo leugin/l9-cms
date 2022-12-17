30a145a9545131e39e5a445e13cd485a6d1756c5
Miguel Quevedo <meqh1922@gmail.com>
2022-12-17 16:30:03 -0500

    feat: add doc

--- app/Data/Dto/SearchAdmin.php
+++ app/Data/Dto/SearchAdmin.php
@@ -4,9 +4,18 @@ namespace App\Data\Dto;
 
 use Illuminate\Support\Str;
 
+/**
+ *
+ */
 class SearchAdmin
 {
 
+    /**
+     * @param string|null $search
+     * @param int|null $id
+     * @param int|null $excludeId
+     * @param int|null $perPage
+     */
     public function __construct(
         public string|null $search = null,
         public int|null $id = null,
@@ -16,16 +25,28 @@ class SearchAdmin
     {
     }
 
+    /**
+     * @param string $key
+     * @return bool
+     */
     public function has(string $key):bool
     {
         return isset($this->{$key});
     }
 
+    /**
+     * @param string $key
+     * @return mixed
+     */
     public function get(string $key):mixed {
         return  !$this->has($key) ? null :  $this->{$key};
     }
 
-    public static function make($attributes = []):self {
+    /**
+     * @param array $attributes
+     * @return static
+     */
+    public static function make(array $attributes = []):self {
         $instance = new self();
         foreach ($attributes as $k => $value) {
             $camelKey = Str::camel($k);
