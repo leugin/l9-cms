d5b53872c6e4a708a21bc34a698ba2bedb1245bf
miguel <meqh1992@gmail.com>
2023-10-05 20:55:41 -0500

    feat: fix error of install

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,7 +8,6 @@ use DateTime;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
